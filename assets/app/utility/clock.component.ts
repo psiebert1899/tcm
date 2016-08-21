594887619e35fbb2d75dc8d76ffe8f33e5c1db78
jgotorbl <jaimegotorblazquez1991@gmail.com>
2016-08-21 18:38:23 -0500

    added clock component

--- assets/app/utility/clock.component.ts
+++ assets/app/utility/clock.component.ts
@@ -1,21 +1,21 @@
-import {Component, Attribute} from "angular2/core";
+import {Component} from "@angular/core";
 
 @Component({
   selector: "my-current-time",
+  providers: [],
   template: `
-      <h2 (updateTime)="updateMyTime()">{{date | date: format}}</h2>
-    `
+    <div>
+      <span>{{date | date: 'MMMMdyyyy h:mm:ss'}}</span>
+    </div>
+  `,
+  directives: []
 })
-export class ClockComponent {
-   private date;
-
-  constructor(@Attribute("format") format) {
-    this.format = format;
-    this.date =  new Date();
-
+export class App {
+  private date;
+  constructor() {
+    this.date = new Date();
     setInterval(() => {
-        this.date =  new Date();
-     }, 1000);
+      this.date = new Date();
+    } , 1000);
   }
-
 }
